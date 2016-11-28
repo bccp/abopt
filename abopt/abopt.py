b88981d9f1f570625c256bd144c004779eec55f1
Yu Feng <rainwoodman@gmail.com>
2016-11-27 19:36:33 -0800

    spelling and other problems.

--- abopt/abopt.py
+++ abopt/abopt.py
@@ -8,6 +8,9 @@ class Optimizer(object):
         self.create = create
         self.addmul = addmul
 
+    def configure(self, **kwargs):
+        self.config.update(kwargs)
+
     def minimize(self, objective, gradient, x0):
         raise NotImplementedError
 
@@ -17,11 +20,29 @@ class Result(object):
         self.ys = ys
         self.gradient = gradient
 
-class GradientDecent(Optimizer):
+class GradientDescent(Optimizer):
     def __init__(self, dot, create, addmul):
         Optimizer.__init__(self, dot, create, addmul)
 
-    def minimize(self, objective, gradient, x0, stepsize, maxsteps):
+    def configure(self,
+                tol=1e-6,
+                gtol=1e-6,
+                maxsteps=1000,
+                **kwargs):
+
+        c = {}
+        c['tol'] = tol
+        c['gtol'] = gtol
+        c['maxsteps'] = maxsteps
+        c.update(kwargs)
+        
+        Optimizer.configure(**c)
+
+    def minimize(self, objective, gradient, x0, stepsize):
+        tol = self.config.get('tol')
+        gtol = self.config.get('gtol')
+        maxsteps = self.config.get('maxsteps')
+
         y0 = objective(x0)
         # FIXME: line search 
         steps = 0
@@ -31,6 +52,8 @@ class GradientDecent(Optimizer):
             y1 = objective(y1)
             x0 = x1
             y0 = y1
+            if abs(y0 - y1) < tol: break
+            if self.dot(x1, x1) < gtol: break
             steps = steps + 1
         dx0 = gradient(x0)
         return Result(x0, y0, dx0)
