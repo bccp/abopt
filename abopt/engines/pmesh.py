7478aeae59181d09dc57e3850193ad9fb8d329e3
Yu Feng <rainwoodman@gmail.com>
2017-03-23 18:46:19 -0700

    add a pmesh engine.

---
+++ abopt/engines/pmesh.py
@@ -0,0 +1,197 @@
+import numpy
+from abopt.vmad2 import ZERO, Engine, statement, programme, CodeSegment
+from pmesh.pm import ParticleMesh, RealField, ComplexField
+
+def create_grid(basepm, shift=0, dtype='f4'):
+    """
+        create uniform grid of particles, one per grid point on the basepm mesh
+        this shall go to pmesh.
+    """
+    ndim = len(basepm.Nmesh)
+    real = basepm.create('real')
+
+    _shift = numpy.zeros(ndim, dtype)
+    _shift[:] = shift
+    # one particle per base mesh point
+    source = numpy.zeros((real.size, ndim), dtype=dtype)
+
+    for d in range(len(real.shape)):
+        real[...] = 0
+        for xi, slab in zip(real.slabs.i, real.slabs):
+            slab[...] = (xi[d] + 1.0 * _shift[d]) * (real.BoxSize[d] / real.Nmesh[d])
+        source[..., d] = real.value.flat
+    return source
+
+def nyquist_mask(factor, v):
+    # any nyquist modes are set to 0 if the transfer function is complex
+    mask = (numpy.imag(factor) == 0) | \
+    numpy.bitwise_and.reduce(list(ii != (ni // 2) for ii, ni in zip(v.i, v.Nmesh)))
+    return factor * mask
+
+class ParticleMeshEngine(Engine):
+    def __init__(self, pm, q=None):
+        self.pm = pm
+        if q is None:
+            q = create_grid(pm)
+        self.q = q
+
+    @statement(aout=['real'], ain=['complex'])
+    def c2r(engine, real, complex):
+        real[...] = complex.c2r()
+
+    @c2r.defvjp
+    def _(engine, _real, _complex):
+        _complex[...] = _real.c2r_gradient()
+
+    @statement(aout=['complex'], ain=['real'])
+    def r2c(engine, complex, real):
+        complex[...] = real.r2c()
+
+    @r2c.defvjp
+    def _(engine, _complex, _real):
+        _real[...] = _complex.r2c_gradient()
+
+    @statement(aout=['complex'], ain=['complex'])
+    def decompress(engine, complex):
+        return
+
+    @decompress.defvjp
+    def _(engine, _complex):
+        _complex.decompress_gradient(out=Ellipsis)
+
+    @staticmethod
+    def _resample_filter(k, v, Neff):
+        k0s = 2 * numpy.pi / v.BoxSize
+        mask = numpy.bitwise_and.reduce([abs(ki) <= Neff//2 * k0 for ki, k0 in zip(k, k0s)])
+        return v * mask
+
+    @statement(aout=['mesh'], ain=['mesh'])
+    def resample(engine, mesh, Neff):
+        mesh.r2c(out=Ellipsis).apply(
+            lambda k, v: engine._resample_filter(k, v, Neff),
+            out=Ellipsis).c2r(out=Ellipsis)
+
+    @resample.defvjp
+    def _(engine, _mesh, Neff):
+        _mesh.c2r_gradient().apply(
+            lambda k, v: engine._resample_filter(k, v, Neff),
+            out=Ellipsis).r2c_gradient(out=Ellipsis)
+
+    @statement(aout=['layout'], ain=['s'])
+    def decompose(engine, layout, s):
+        x = s + engine.q
+        pm = engine.pm
+        layout[...] = pm.decompose(x)
+
+    @decompose.defvjp
+    def _(engine, _layout, _s):
+        _s[...] = ZERO
+
+    @statement(aout=['mesh'], ain=['s', 'layout'])
+    def paint(engine, s, mesh, layout):
+        pm = engine.pm
+        x = s + engine.q
+        mesh[...] = pm.create(mode='real')
+        N = pm.comm.allreduce(len(x))
+        mesh[...].paint(x, layout=layout, hold=False)
+        # to have 1 + \delta on the mesh
+        mesh[...][...] *= 1.0 * pm.Nmesh.prod() / N
+
+    @paint.defvjp
+    def _(engine, _s, _mesh, s, layout, _layout):
+        pm = engine.pm
+        _layout[...] = ZERO
+        x = s + engine.q
+        N = pm.comm.allreduce(len(x))
+        _s[...], junk = _mesh.paint_gradient(x, layout=layout, out_mass=False)
+        _s[...][...] *= 1.0 * pm.Nmesh.prod() / N
+
+    @statement(aout=['value'], ain=['s', 'mesh', 'layout'])
+    def readout(engine, value, s, mesh, layout):
+        pm = engine.pm
+        x = s + engine.q
+        N = pm.comm.allreduce(len(x))
+        value[...] = mesh.readout(x, layout=layout)
+
+    @readout.defvjp
+    def _(engine, _value, _s, _mesh, s, layout, mesh):
+        pm = engine.pm
+        x = s + engine.q
+        _mesh[...], _s[...] = mesh.readout_gradient(x, _value, layout=layout)
+
+    @statement(aout=['complex'], ain=['complex'])
+    def transfer(engine, complex, tf):
+        complex.apply(lambda k, v: nyquist_mask(tf(k), v) * v, out=Ellipsis)
+
+    @transfer.defvjp
+    def _(engine, tf, _complex):
+        _complex.apply(lambda k, v: nyquist_mask(numpy.conj(tf(k)), v) * v, out=Ellipsis)
+
+    @statement(aout=['r'], ain=['field'])
+    def norm(engine, field, r, metric=None):
+        if isinstance(field, ComplexField):
+            r[...] = field.cnorm(metric=metric)
+        else:
+            r[...] = field.cnorm()
+
+    @norm.defvjp
+    def _(engine, _field, _r, metric, field):
+        if isinstance(field, ComplexField):
+            _field[...] = field.cnorm_gradient(_r, metric=metric)
+        else:
+            _field[...] = field * 2
+
+    @statement(aout=['residual'], ain=['model'])
+    def residual(engine, model, data, sigma, residual):
+        d = model - data
+        d[...] /= sigma
+        residual[...] = d
+
+    @residual.defvjp
+    def _(engine, _model, _residual, data, sigma):
+        g = _residual.copy()
+        g[...] /= sigma
+        _model[...] = g
+
+    @statement(ain=['attribute', 'value'], aout=['attribute'])
+    def assign_component(engine, attribute, value, dim):
+        attribute[..., dim] = value
+
+    @assign_component.defvjp
+    def _(engine, _attribute, _value, dim):
+        _value[...] = _attribute[..., dim]
+
+    @statement(ain=['x'], aout=['y'])
+    def assign(engine, x, y):
+        y[...] = x.copy()
+
+    @assign.defvjp
+    def _(engine, _y, _x):
+        _x[...] = _y
+
+    @programme(aout=['force'], ain=['density', 's'])
+    def force(engine, force, density, s):
+        code = CodeSegment(engine)
+        code.defaults['force'] = numpy.zeros_like(engine.q)
+        code.decompose(s=s, layout='layout')
+        for d in range(engine.pm.ndim):
+            def tf(k):
+                k2 = sum(ki ** 2 for ki in k)
+                mask = k2 == 0
+                k2[mask] = 1.0
+                return 1j * k[d] / k2 * ~mask
+            code.assign(x='density', y='complex')
+            code.transfer(complex='complex', tf=tf)
+            code.c2r(complex='complex', real='real')
+            code.readout(value='force1', mesh='real', s=s, layout='layout')
+            code.assign_component(attribute='force', dim=d, value='force1')
+        return code
+
+    @statement(ain=['x'], aout=['y'])
+    def to_scalar(engine, x, y):
+        y[...] = engine.pm.comm.allreduce((x[...] ** 2).sum(dtype='f8'))
+
+    @to_scalar.defvjp
+    def _(engine, _y, _x, x):
+        _x[...] = (2 * _y) * x
+
---
+++ abopt/tests/test_pmesh.py
@@ -0,0 +1,167 @@
+from __future__ import print_function
+from abopt.vmad2 import CodeSegment, Engine, statement, programme, ZERO, logger
+
+from numpy.testing import assert_raises, assert_array_equal, assert_allclose
+from numpy.testing.decorators import skipif
+import numpy
+import logging
+
+from abopt.engines.pmesh import ParticleMeshEngine, ParticleMesh, RealField, ComplexField
+
+logger.setLevel(level=logging.WARNING)
+
+try:
+    from abopt.engines.pmesh import ParticleMeshEngine, ParticleMesh, RealField, ComplexField
+    pm = ParticleMesh(BoxSize=1.0, Nmesh=(4, 4), dtype='f8')
+except ImportError:
+    pm = None
+
+@skipif(pm == None)
+def test_compute():
+    def transfer(k): return 2.0
+    engine = ParticleMeshEngine(pm)
+    code = CodeSegment(engine)
+    code.r2c(real='r', complex='c')
+    code.transfer(complex='c', tf=transfer)
+    code.c2r(complex='c', real='r')
+    code.norm(field='r', r='sum', metric=None)
+
+    field = pm.generate_whitenoise(seed=1234).c2r()
+
+    norm = code.compute('sum', init={'r': field})
+    assert_allclose(norm, field.cnorm() * 4)
+
+@skipif(pm == None)
+def test_gradient():
+    def transfer(k):
+        k2 = sum(ki **2 for ki in k)
+        k2[k2 == 0] = 1.0
+#        return 1 / k2
+        return 2.0
+    engine = ParticleMeshEngine(pm)
+    code = CodeSegment(engine)
+    code.r2c(real='r', complex='c')
+    code.transfer(complex='c', tf=transfer)
+    code.c2r(complex='c', real='r')
+    code.norm(field='r', r='sum', metric=None)
+
+    field = pm.generate_whitenoise(seed=1234).c2r()
+
+    norm = code.compute('sum', init={'r': field})
+    assert_allclose(norm, field.cnorm() * 4)
+    norm, _r = code.compute_with_gradient(('sum', '_r'), init={'r': field}, ginit={'_sum': 1.0})
+    assert_allclose(_r, field * 4 * 2)
+
+@skipif(pm == None)
+def test_force():
+    def transfer(k): return 2.0
+    engine = ParticleMeshEngine(pm)
+    code = CodeSegment(engine)
+    code.r2c(real='r', complex='c')
+    code.force(density='c', s='s', force='force')
+
+    s = engine.q * 0.0 + 0.1
+    field = pm.generate_whitenoise(seed=1234).c2r()
+
+    cshape = engine.pm.comm.allreduce(engine.q.shape[0]), engine.q.shape[1]
+
+    check_grad(code, 'force', 's', init={'r': field, 's': s}, eps=1e-4,
+                cshape=cshape, cperturb=perturb_pos, cget=get_pos,
+                rtol=1e-2)
+
+    check_grad(code, 'force', 'r', init={'r': field, 's': s}, eps=1e-4,
+                cshape=field.cshape, cperturb=perturb_field, cget=get_field,
+                rtol=1e-2)
+
+@skipif(pm == None)
+def test_paint():
+    engine = ParticleMeshEngine(pm)
+    code = CodeSegment(engine)
+    s = numpy.random.uniform(size=engine.q.shape) * 0.1
+
+    cshape = engine.pm.comm.allreduce(engine.q.shape[0]), engine.q.shape[1]
+    code.decompose(s='s', layout='layout')
+    code.paint(s='s', mesh='density', layout='layout')
+
+    check_grad(code, 'density', 's', init={'s': s}, eps=1e-4,
+                cshape=cshape, cperturb=perturb_pos, cget=get_pos,
+                rtol=1e-2)
+
+@skipif(pm == None)
+def test_readout():
+    engine = ParticleMeshEngine(pm)
+    code = CodeSegment(engine)
+    s = numpy.random.uniform(size=engine.q.shape) * 0.1
+    cshape = engine.pm.comm.allreduce(engine.q.shape[0]), engine.q.shape[1]
+
+    field = pm.generate_whitenoise(seed=1234).c2r()
+
+    code.decompose(s='s', layout='layout')
+    code.readout(s='s', mesh='density', layout='layout', value='value')
+
+    check_grad(code, 'value', 's', init={'density' : field, 's': s}, eps=1e-4,
+                cshape=cshape, cperturb=perturb_pos, cget=get_pos,
+                rtol=1e-2)
+
+    check_grad(code, 'value', 'density', init={'density' : field, 's': s}, eps=1e-4,
+                cshape=field.cshape, cperturb=perturb_field, cget=get_field,
+                rtol=1e-2)
+
+def check_grad(code, yname, xname, init, eps, cshape, cperturb, cget, rtol):
+    code = code.copy()
+    code.to_scalar(x=yname, y='y')
+
+    y, tape = code.compute('y', init=init, return_tape=True)
+    gradient = code.gradient(tape)
+    _x = gradient.compute('_' + xname, init={'_y' : 1.0})
+
+    center = init[xname]
+    init2 = init.copy()
+    for index in numpy.ndindex(*cshape):
+        x1 = cperturb(code.engine, center, index, eps)
+        x0 = cperturb(code.engine, center, index, -eps)
+        analytic = cget(code.engine, _x, index)
+        init2[xname] = x1
+        y1 = code.compute('y', init2)
+        init2[xname] = x0
+        y0 = code.compute('y', init2)
+        #print(y1, y0, y1 - y0, get_pos(code.engine, _x, index) * 2 * eps)
+        assert_allclose(y1 - y0, get_pos(code.engine, _x, index) * 2 * eps, rtol=rtol)
+
+
+def get_field(engine, real, index):
+    return real.cgetitem(index)
+
+def perturb_field(engine, real, index, eps):
+    old = real.cgetitem(index)
+    r1 = real.copy()
+    r1.csetitem(index, old + eps)
+    return r1
+
+def perturb_pos(engine, pos, ind, eps):
+    comm = engine.pm.comm
+    pos = pos.copy()
+    start = sum(comm.allgather(pos.shape[0])[:comm.rank])
+    end = sum(comm.allgather(pos.shape[0])[:comm.rank + 1])
+    if ind[0] >= start and ind[0] < end:
+        old = pos[ind[0] - start, ind[1]]
+        coord = pos[ind[0]-start].copy()
+        pos[ind[0] - start, ind[1]] = old + eps
+        new = pos[ind[0] - start, ind[1]]
+    else:
+        old = 0
+        new = 0
+        coord = 0
+    diff = comm.allreduce(new - old)
+
+    return pos
+
+def get_pos(engine, pos, ind):
+    comm = engine.pm.comm
+    start = sum(comm.allgather(pos.shape[0])[:comm.rank])
+    end = sum(comm.allgather(pos.shape[0])[:comm.rank + 1])
+    if ind[0] >= start and ind[0] < end:
+        old = pos[ind[0] - start, ind[1]]
+    else:
+        old = 0
+    return comm.allreduce(old)
