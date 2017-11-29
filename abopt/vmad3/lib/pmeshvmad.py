d34ca395e86fd41a8c66ad8bd522767f5a24cb3b
Yu Feng <rainwoodman@gmail.com>
2017-11-29 00:30:31 -0800

    Add a few pmesh operators.

---
+++ abopt/vmad3/lib/pmeshvmad.py
@@ -0,0 +1,132 @@
+from abopt.vmad3 import operator
+from pmesh.pm import ParticleMesh
+import numpy
+
+@operator
+class to_scalar:
+    ain = {'x' : 'RealField'}
+    aout = {'y' : '*'}
+
+    def apl(self, x):
+        return dict(y = x.cnorm())
+
+    def vjp(self, _y, x):
+        return dict(_x= x * (2 * _y))
+
+    def jvp(self, x_, x):
+        return dict(y_ = x.cdot(x_) * 2)
+
+@operator
+class r2c:
+    ain = {'x' : 'RealField'}
+    aout = {'y' : 'ComplexField'}
+
+    def apl(self, x):
+        return dict(y=x.r2c())
+    def vjp(self, _y):
+        return dict(_x=_y.r2c_vjp())
+    def jvp(self, x_):
+        return dict(y_=x_.r2c())
+
+@operator
+class c2r:
+    ain = {'x' : 'ComplexField'}
+    aout = {'y' : 'RealField'}
+
+    def apl(self, x):
+        return dict(y=x.c2r())
+    def vjp(self, _y):
+        return dict(_x=_y.c2r_vjp())
+    def jvp(self, x_):
+        return dict(y_=x_.c2r())
+
+def nyquist_mask(v):
+    mask = ~numpy.bitwise_and.reduce([(ii == 0) | (ii == ni // 2) for ii, ni in zip(v.i, v.Nmesh)])
+    return 1.0 * mask
+
+@operator
+class apply_transfer:
+    ain = {'c' : 'ComplexField'}
+    aout = {'c' : 'ComplexField'}
+
+    def apl(self, x, tf):
+        filter = lambda k, v: nyquist_mask(v) * tf(k) * v
+        return dict(y=x.apply(filter))
+
+    def vjp(self, _y):
+        filter = lambda k, v: nyquist_mask(v) * numpy.conj(tf(k)) * v
+        return dict(_x=_y.apply(filter))
+
+    def jvp(self, x_):
+        filter = lambda k, v: nyquist_mask(v) * tf(k) * v
+        return dict(y_=x_.apply(filter))
+
+@operator
+class paint:
+    aout = {'mesh' : 'RealField'}
+    ain =  {'x': 'ndarray',
+           'layout': 'Layout',
+           'mass' : 'ndarray'
+           }
+
+    def apl(self, x, mass, layout, pm):
+        N = pm.comm.allreduce(len(x))
+        mesh = pm.paint(x, mass=mass, layout=layout, hold=False)
+        # 1 + delta
+        mesh[...] *= 1.0 * pm.Nmesh.prod() / N
+        return dict(mesh=mesh)
+
+    def vjp(self, _mesh, x, mass, layout, pm):
+        N = pm.comm.allreduce(len(x))
+        _x, _mass = pm.paint_vjp(_mesh, x, layout=layout, mass=mass)
+        _x[...] *= 1.0 * pm.Nmesh.prod() / N
+        _mass[...] *= 1.0 * pm.Nmesh.prod() / N
+        return dict(
+            _layout = 0,
+            _x=_x,
+            _mass=_mass)
+
+    def jvp(self, x_, x, layout, mass, layout_, mass_, pm):
+        if x_ is 0: x_ = None
+        if mass_ is 0: mass_ = None # force cast it to a scalar 0, so make it None
+        mesh_ = pm.paint_jvp(x, v_mass=mass_, mass=mass, v_pos=x_, layout=layout)
+
+        return dict(mesh_=mesh_)
+
+@operator
+class readout:
+    aout = {'value' : 'ndarray'}
+
+    ain = {'x': 'ndarray',
+        'mesh': 'RealField',
+      'layout' : 'Layout'}
+
+    def apl(self, mesh, x, layout, resampler=None):
+        N = mesh.pm.comm.allreduce(len(x))
+        value = mesh.readout(x, layout=layout, resampler=resampler)
+        return dict(value=value)
+
+    def vjp(self, _value, x, layout, mesh, resampler=None):
+        _mesh, _x = mesh.readout_vjp(x, _value, layout=layout, resampler=resampler)
+        return dict(_mesh=_mesh, _x=_x, _layout=0)
+
+    def jvp(self, x_, mesh_, x, layout, layout_, mesh, resampler=None):
+        if mesh_ is 0: mesh_ = None
+        if x_ is 0: x_ = None
+        value_ = mesh.readout_jvp(x, v_self=mesh_, v_pos=x_, layout=layout, resampler=resampler)
+        return dict(value_=value_)
+
+
+@operator
+class decompose:
+    aout={'layout' : 'Layout'}
+    ain={'x': 'ndarray'}
+
+    def apl(self, x, pm):
+        return dict(layout=pm.decompose(x))
+
+    def vjp(engine, _layout, _x, pm):
+        return dict(_x=0)
+
+    def jvp(engine, layout_, x_):
+        return dict(layout_=0)
---
+++ abopt/vmad3/lib/tests/test_pmeshvmad.py
@@ -0,0 +1,100 @@
+from __future__ import print_function
+from pprint import pprint
+from abopt.vmad3.lib import pmeshvmad, linalg
+import numpy
+
+from abopt.vmad3.testing import BaseScalarTest
+from mpi4py import MPI
+
+pm = pmeshvmad.ParticleMesh(Nmesh=[4, 4], BoxSize=8.0, comm=MPI.COMM_SELF)
+
+from pmesh.pm import RealField, ComplexField
+
+def create_bases(x):
+    bases = numpy.eye(x.size).reshape([-1] + list(x.shape))
+    if isinstance(x, RealField):
+        # FIXME: remove this after pmesh 0.1.36
+        def create_field(pm, data):
+            real = pm.create(mode='real')
+            real[...] = data
+            return real
+        return [create_field(pm, i) for i in bases]
+    else:
+        return [i for i in bases]
+
+class Test_r2c_c2r(BaseScalarTest):
+    to_scalar = pmeshvmad.to_scalar
+
+    x = pm.generate_whitenoise(seed=300, unitary=True, mode='real')
+    y = x.cnorm()
+    x_ = create_bases(x)
+
+    def model(self, x):
+        c = pmeshvmad.r2c(x)
+        r = pmeshvmad.c2r(c)
+        return r
+
+#    def teardown(self):
+#        print(self.y_)
+
+class Test_paint_x(BaseScalarTest):
+    to_scalar = pmeshvmad.to_scalar
+
+    mesh = pm.generate_whitenoise(seed=300, unitary=True, mode='real')
+
+    x = pm.generate_uniform_particle_grid(shift=0.1)
+    y = (1 + mesh).cnorm()
+    x_ = create_bases(x)
+
+    epsilon = 1e-3
+
+    def model(self, x):
+        y = pmeshvmad.paint(x, layout=None, mass=1.0, pm=pm)
+        y = linalg.add(y, self.mesh) # biasing a bit to get non-zero derivatives.
+        return y
+
+class Test_paint_mass(BaseScalarTest):
+    to_scalar = pmeshvmad.to_scalar
+
+    mesh = pm.generate_whitenoise(seed=300, unitary=True, mode='real')
+
+    pos = pm.generate_uniform_particle_grid(shift=0.1)
+    x = numpy.ones(len(pos))
+    y = x.sum()
+    x_ = create_bases(x)
+
+    epsilon = 1e-3
+
+    def model(self, x):
+        y = pmeshvmad.paint(self.pos, layout=None, mass=x, pm=pm)
+        return y
+
+class Test_readout_x(BaseScalarTest):
+    to_scalar = linalg.to_scalar
+
+    mesh = pm.generate_whitenoise(seed=300, unitary=True, mode='real')
+
+    x = pm.generate_uniform_particle_grid(shift=0.5)
+    y = (mesh.readout(x) ** 2).sum()
+    x_ = create_bases(x)
+
+    epsilon = 1e-3
+
+    def model(self, x):
+        y = pmeshvmad.readout(self.mesh, x, layout=None, pm=pm)
+        return y
+
+class Test_readout_mesh(BaseScalarTest):
+    to_scalar = linalg.to_scalar
+
+    x = pm.generate_whitenoise(seed=300, unitary=True, mode='real')
+
+    pos = pm.generate_uniform_particle_grid(shift=0.5)
+    y = (x.readout(pos) ** 2).sum()
+    x_ = create_bases(x)
+
+    epsilon = 1e-3
+
+    def model(self, x):
+        y = pmeshvmad.readout(x, self.pos, layout=None, pm=pm)
+        return y
