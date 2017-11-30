87007c68fc478627ad669d8bec2726af7d71c6c6
Yu Feng <rainwoodman@gmail.com>
2017-11-30 14:32:15 -0800

    shuffling primitive.py, adding docstrings.

--- abopt/vmad3/core/primitive.py
+++ abopt/vmad3/core/primitive.py
@@ -3,49 +3,34 @@ import weakref
 from .error import InferError, UnpackError, OverwritePrecaution, MissingArgument, BrokenPrimitive, BadArgument
 from .symbol import Symbol, Literal, List
 
-def make_symbol(model, var):
-    if isinstance(var, Primitive):
-        # unpack the primitive result
-        #
-        # see __iter__ if explict unpack (a, b = primitive(...))
-        # is used.
+def make_symbol(model, obj):
+    """ Make a symbol out of an input Python object.
 
-        if len(var.varout) > 1:
+    """
+    if isinstance(obj, Primitive):
+        # unpack single output primitives
+        # allows a = primitive(...)
+        # also see __iter__ for explict unpacking (a, b = primitive(...))
+        if len(obj.varout) > 1:
             raise UnpackError("More than one output variable, need to unpack them")
-        var = next(iter(var.varout.values()))
+        obj = next(iter(obj.varout.values()))
 
-    if isinstance(var, (list, tuple)):
-        var = List(model, [make_symbol(model, v) for v in var])
+        # FIXME: Alternatively, make Primitive a subclass of Symbol.
+        # But then some Primitives shall be subclasses of List.
+        # the class hierarchy is a bit hairy and sounds like we will
+        # in the end always need to deal with single output and multi output
+        # primitives specially anyways. Shall give it an attempt.
 
-    if not isinstance(var, Symbol):
-        var = Literal(model, var)
+    if isinstance(obj, (list, tuple)):
+        obj = List(model, [make_symbol(model, i) for i in obj])
 
-    return var
+    # still not a Symbol? Must be some raw python object
+    # intended to be used as a Literal
+    if not isinstance(obj, Symbol):
+        obj = Literal(model, obj)
 
-def _infer_model(var):
-    if isinstance(var, Primitive):
-        var = next(iter(var.varout.values()))
+    return obj
 
-    if isinstance(var, Symbol):
-        model = var.model
-        return model
-
-    if isinstance(var, (list, tuple)):
-        for v in var:
-            model = _infer_model(v)
-            if model is not None:
-                return model
-
-    return None
-
-def check_var_references(var):
-    if isinstance(var, List):
-        for v in var.value:
-            check_var_references(v)
-        return
-
-    if len(var.references) != 0:
-        raise OverwritePrecaution("Overwritting used symbols is not supported. Because it breaks vjp.")
 
 class Primitive(object):
     """ Primitives are building blocks of models.
@@ -57,56 +42,23 @@ class Primitive(object):
 
     """
 
-    def _scan_kwargs(self, kwargs):
-        kls = type(self)
-        model = None
-
-        for argname in kls.ain:
-            if not argname in kwargs: raise MissingArgument("input argument '%s' not provided" % argname)
-
-            var = kwargs[argname]
-            model = _infer_model(var)
-            if model is not None:
-                self._model = weakref.ref(model)
-                return model
-
-        for argname in kls.aout:
-            if argname not in kwargs: continue
-            var = kwargs[argname]
-
-            model = _infer_model(var)
-            if model is not None:
-                self._model = weakref.ref(model)
-                return model
-
-        raise InferError("Cannot infer model from variables -- try to mark at least one literal argument explicitly as Literal")
-
     def __init__(self, *args, **kwargs):
+        """ Creating a node and append it to the model's execution graph
 
+            The model is inferred from the input arguments
+        """
         kls = type(self)
 
-        # assert the primitive is properly defined.
-        for attr in ['ain', 'aout', 'impl', 'func', 'argnames', 'operator']:
-            if not hasattr(kls, attr):
-                raise BrokenPrimitive("primitive class attribute '%s' is not defined" % attr)
+        _check_primitive_class(kls)
 
         self.varin = {}
         self.varout = {}
         self.hyper_args = {}
 
-        kwargs = kwargs.copy() # will modify
+        kwargs = _parse_args(kls, args, kwargs)
 
-        # first attempt to map args into kwargs
-        if len(args) > len(kls.argnames):
-            raise BadArgument("Argument list longer than total number of args")
+        model = _find_model(kls, kwargs)
 
-        for argname, arg in zip(kls.argnames, args):
-            if argname in kwargs:
-                raise BadArgument("argument %s already provided as keyword" % argname)
-
-            kwargs[argname] = arg
-
-        model = self._scan_kwargs(kwargs)
         self._name = model.unique_name(kls.__name__)
 
         for argname in kls.ain:
@@ -126,9 +78,12 @@ class Primitive(object):
             else:
                 var = kwargs[argname]
                 var = make_symbol(model, kwargs[argname])
+
                 # already given a symbol, overwrite it
-                # but this doesn't work for gradients
-                check_var_references(var)
+                # but this doesn't work for gradients / tape
+                # so we die here
+                _check_var_references(var)
+
                 # make a new symbol of the same name
                 # var = model.define(var.name)
 
@@ -139,11 +94,19 @@ class Primitive(object):
             if k not in kls.ain and k not in kls.aout:
                 self.hyper_args[k] = v
 
+        # append self to the model.
         model.append(self)
 
     def __iter__(self):
-        """ for unpacking the results in model building """
-        for argname in self.aout:
+        """ for unpacking the varout during model construction, e.g.
+
+            .. code::
+
+                a, b, c = split_three_way(x)
+
+        """
+
+        for argname in type(self).aout:
             yield self.varout[argname]
 
     @property
@@ -154,7 +117,14 @@ class Primitive(object):
         return "%s(%s=>%s)" % (self._name, self.varin, self.varout)
 
     def call(self, **kwargs):
-        """ call the implementation function of the primitive """
+        """ call the implementation function of the primitive;
+
+            invoked by the Context
+
+            kwargs: the arguments that goes into the impl function
+
+            Returns: dict, result for each varout.
+        """
         r = type(self).impl(self, **kwargs)
 
         # allow returning without using a dict
@@ -166,9 +136,91 @@ class Primitive(object):
         return r
 
     def record(self, kwargs):
-        """ generate the kwargs that goes into the tape """
+        """ generate the kwargs that goes into the tape;
+            default is to record the entire kwargs.
+
+            Sometimes we do not need the entire kwargs; e.g.
+            for linear operators we only need enough information to create
+            the output array of the back-prop gradient
+            but we don't need the actual parameters.
+
+            invoked by the Context.
+
+            kwargs: the arguments that goes into the impl function
+
+            Returns: dict that goes into the tape
+        """
         if type(self).record_impl is not None:
             return type(self).record_impl(self, **kwargs)
         else:
             return kwargs
 
+
+def _infer_model(var):
+    if isinstance(var, Primitive):
+        var = next(iter(var.varout.values()))
+
+    if isinstance(var, Symbol):
+        model = var.model
+        return model
+
+    if isinstance(var, (list, tuple)):
+        for v in var:
+            model = _infer_model(v)
+            if model is not None:
+                return model
+
+    return None
+
+def _check_primitive_class(kls):
+    # assert the primitive is properly defined.
+    for attr in ['ain', 'aout', 'impl', 'func', 'argnames', 'operator', 'record_impl']:
+        if not hasattr(kls, attr):
+            raise BrokenPrimitive("primitive class attribute '%s' is not defined" % attr)
+
+def _check_var_references(var):
+    if isinstance(var, List):
+        for v in var.value:
+            _check_var_references(v)
+        return
+
+    if len(var.references) != 0:
+        raise OverwritePrecaution("Overwritting used symbols is not supported. Because it breaks vjp.")
+
+def _parse_args(kls, args, kwargs):
+    """ map arguments give as args and kwargs to argnames.
+    """
+    kwargs = kwargs.copy() # will modify
+
+    # first attempt to map args into kwargs
+    if len(args) > len(kls.argnames):
+        raise BadArgument("Argument list longer than total number of args")
+
+    for argname, arg in zip(kls.argnames, args):
+        if argname in kwargs:
+            raise BadArgument("argument %s already provided as keyword" % argname)
+
+        kwargs[argname] = arg
+
+    return kwargs
+
+def _find_model(kls, kwargs):
+    model = None
+
+    for argname in kls.ain:
+        if not argname in kwargs: raise MissingArgument("input argument '%s' not provided" % argname)
+
+        var = kwargs[argname]
+        model = _infer_model(var)
+        if model is not None:
+            return model
+
+    for argname in kls.aout:
+        if argname not in kwargs: continue
+        var = kwargs[argname]
+
+        model = _infer_model(var)
+        if model is not None:
+            return model
+
+    raise InferError("Cannot infer model from variables -- try to mark at least one literal argument explicitly as Literal")
